b25a3e75b93ff2650c2700435be2817c4bc88121
Godspower Eze <Godspowereze260@gmail.com>
2024-03-14 08:58:25 +0100

    feat: update on SHA-3

--- py/commitments/hashing/sha3/utils.py
+++ py/commitments/hashing/sha3/utils.py
@@ -1,3 +1,11 @@
+from bitarray import bitarray
+
+
+def pad(message: bytes) -> bitarray:
+    a = bitarray()
+    a.frombytes(message)
+    return a
+
 
 def one_d_to_three_d():
     pass
@@ -5,3 +13,8 @@ def one_d_to_three_d():
 
 def three_d_to_one_d():
     pass
+
+
+message = bytes.fromhex("aebcdf")
+padded_message = pad(message)
+print(padded_message)
