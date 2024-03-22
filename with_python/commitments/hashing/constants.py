6c86b5a5904d8ee50dad795ba03ef20996aa20ee
Godspower Eze <Godspowereze260@gmail.com>
2024-03-22 09:03:32 +0100

    feat: updated SHA-3

--- with_python/commitments/hashing/constants.py
+++ with_python/commitments/hashing/constants.py
@@ -1,4 +1,8 @@
 BITS_PER_BYTE = 8
 MAX_32_BIT_VALUE = 0xFFFFFFFF
 MAX_64_BIT_VALUE = 0xFFFFFFFFFFFFFFFF
-SHA3_BITS = 200  # in bytes
+
+# SHA-3
+l = 6  # possible values of l = {0, 1, 2, 3, 4, 5, 6} but 6 for SHA-3
+STATE_SIZE = 25 * (2 ** l)  # state size for SHA-3 which is 1600 bits
+ROUNDS = 12 + (2 * l)
--- with_python/commitments/hashing/sha3/utils.py
+++ with_python/commitments/hashing/sha3/utils.py
@@ -1,4 +1,5 @@
 from bitarray import bitarray
+import numpy as np
 
 
 def pad(message: bytes, rate: int) -> bitarray:
@@ -30,23 +31,43 @@ def pad(message: bytes, rate: int) -> bitarray:
     # add domain separation bits
     a.extend([0, 1])
 
+    #
     length = len(a)
+    padding = []
     j = (-(length) - 2) % rate
-    a.append(1)
+    padding.append(1)
     zeros = [0 for _ in range(0, j)]
-    a.extend(zeros)
-    a.append(1)
+    padding.extend(zeros)
+    padding.append(1)
+    a.extend(padding)
     return a
 
 
-def one_d_to_three_d():
-    pass
+def one_d_to_three_d(bits: bitarray) -> np.ndarray:
+    """
+    implemented according to the spec(FIPS PUB 202), section 3.1.2
+    """
+    out = np.zeros((5, 5, 64), dtype=int)
+    for i in range(5):
+        for j in range(5):
+            for k in range(64):
+                out[i][j][k] = bits[64*(5*j + i) + k]
+    return out
 
 
-def three_d_to_one_d():
-    pass
+def three_d_to_one_d(bits_box: np.ndarray) -> bitarray:
+    """
+    implemented according to the spec(FIPS PUB 202), section 3.1.3
+    """
+    out = np.zeros(1600, dtype=int)  # Initialize empty array of size 1600
+    for i in range(5):
+        for j in range(5):
+            for k in range(64):
+                out[64*(5*j+i)+k] = bits_box[i][j][k]
+    return bitarray(out.tolist())
 
 
 message = bytes.fromhex("aebcdf")
-padded_message = pad(message, 1088)
-print(len(padded_message))
+padded_message = pad(message, 1600)
+d = one_d_to_three_d(padded_message)
+e = three_d_to_one_d(d)
