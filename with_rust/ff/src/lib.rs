6ee08bf52268f46f5c9d5fb3f4963408cef6585b
Godspower Eze <Godspowereze260@gmail.com>
2024-03-22 07:33:29 +0100

    feat: updated ff

--- with_rust/ff/src/lib.rs
+++ with_rust/ff/src/lib.rs
@@ -1,11 +1,13 @@
-use std::ops::{Rem, Add, Sub, Mul, Div};
+use std::ops::{Add, Mul, Rem, Sub};
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct FF {
+pub struct FFE {
     // modulus
     n: usize,
     // element
-    value: usize
+    value: usize,
+    // generator
+    g: usize,
 }
 
 struct ISize {
@@ -15,7 +17,7 @@ struct ISize {
 #[derive(Debug)]
 pub enum Error {
     InvalidModulus,
-    DifferentModulus
+    DifferentModulus,
 }
 
 impl Rem<usize> for ISize {
@@ -32,35 +34,39 @@ impl Rem<usize> for ISize {
     }
 }
 
-impl Add for FF {
-
+impl Add for FFE {
     type Output = Result<Self, Error>;
 
     fn add(self, rhs: Self) -> Self::Output {
         if self.n != rhs.n {
             return Err(Error::DifferentModulus);
         } else {
-            Ok(Self {value: (self.value + rhs.value) % self.n, n: self.n})
+            Ok(Self {
+                value: (self.value + rhs.value) % self.n,
+                n: self.n,
+                g: self.g,
+            })
         }
     }
-    
 }
 
-impl Mul for FF {
-
+impl Mul for FFE {
     type Output = Result<Self, Error>;
 
     fn mul(self, rhs: Self) -> Self::Output {
         if self.n != rhs.n {
             return Err(Error::DifferentModulus);
         } else {
-            Ok(Self {value: (self.value * rhs.value) % self.n, n: self.n})
+            Ok(Self {
+                value: (self.value * rhs.value) % self.n,
+                n: self.n,
+                g: self.g,
+            })
         }
     }
 }
 
-impl Sub for FF {
-
+impl Sub for FFE {
     type Output = Result<Self, Error>;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -69,109 +75,225 @@ impl Sub for FF {
         } else {
             // TODO: investigate safety of conversion
             let (sub, _) = self.value.overflowing_sub(rhs.value);
-            Ok(Self {value: ISize {value: sub as isize} % self.n, n: self.n})
+            Ok(Self {
+                value: ISize {
+                    value: sub as isize,
+                } % self.n,
+                n: self.n,
+                g: self.g,
+            })
         }
     }
-
 }
 
-// impl Div for FF {
-    
-// }
-
-impl FF {
-
-    pub fn new(value: isize, n: usize) -> Result<Self, Error> {
+impl FFE {
+    pub fn new(value: isize, g: usize, n: usize) -> Result<Self, Error> {
         if n == 0 || n == 1 {
             return Err(Error::InvalidModulus);
         }
-        Ok(Self{value: ISize {value} % n, n: n})
+        Ok(Self {
+            value: ISize { value } % n,
+            n,
+            g,
+        })
     }
 
-    pub fn zero(n: usize) -> Self {
-        FF{n, value: 0}
+    pub fn zero(&self) -> Self {
+        FFE {
+            n: self.n,
+            g: self.g,
+            value: 0,
+        }
     }
 
-    pub fn one(n: usize) -> Self {
-        FF{n, value: 1}
+    pub fn one(&self) -> Self {
+        FFE {
+            n: self.n,
+            g: self.g,
+            value: 1,
+        }
+    }
+
+    pub fn get_element(&self) -> usize {
+        self.n
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::FF;
+    use crate::FFE;
 
     #[test]
     fn new() {
-        assert_eq!(FF::new(4, 5).unwrap(), FF{value: 4, n: 5});
-        assert_eq!(FF::new(8, 5).unwrap(), FF{value: 3, n: 5});
-        assert_eq!(FF::new(77, 5).unwrap(), FF{value: 2, n: 5});
+        assert_eq!(
+            FFE::new(4, 1, 5).unwrap(),
+            FFE {
+                value: 4,
+                n: 5,
+                g: 1
+            }
+        );
+        assert_eq!(
+            FFE::new(8, 1, 5).unwrap(),
+            FFE {
+                value: 3,
+                n: 5,
+                g: 1
+            }
+        );
+        assert_eq!(
+            FFE::new(77, 1, 5).unwrap(),
+            FFE {
+                value: 2,
+                n: 5,
+                g: 1
+            }
+        );
         // negative value
-        assert_eq!(FF::new(-6, 5).unwrap(), FF{value: 4, n: 5});
-        assert_eq!(FF::new(-20, 5).unwrap(), FF{value: 0, n: 5});
-        assert_eq!(FF::new(-89, 5).unwrap(), FF{value: 1, n: 5});
+        assert_eq!(
+            FFE::new(-6, 1, 5).unwrap(),
+            FFE {
+                value: 4,
+                n: 5,
+                g: 1
+            }
+        );
+        assert_eq!(
+            FFE::new(-20, 1, 5).unwrap(),
+            FFE {
+                value: 0,
+                n: 5,
+                g: 1
+            }
+        );
+        assert_eq!(
+            FFE::new(-89, 1, 5).unwrap(),
+            FFE {
+                value: 1,
+                n: 5,
+                g: 1
+            }
+        );
     }
 
     #[test]
     fn zero() {
-        assert_eq!(FF::zero(5), FF{value: 0, n: 5});
+        let ff_1 = FFE::new(10, 1, 5).unwrap();
+        assert_eq!(
+            ff_1.zero(),
+            FFE {
+                value: 0,
+                n: 5,
+                g: 1
+            }
+        );
     }
-    
+
     #[test]
     fn one() {
-        assert_eq!(FF::one(5), FF{value: 1, n: 5});
+        let ff_1 = FFE::new(10, 1, 5).unwrap();
+        assert_eq!(
+            ff_1.one(),
+            FFE {
+                value: 1,
+                n: 5,
+                g: 1
+            }
+        );
     }
 
     #[test]
     fn add() {
-        let ff_1 = FF::new(19, 5).unwrap();
-        let ff_2 = FF::new(10, 5).unwrap();
+        let ff_1 = FFE::new(19, 1, 5).unwrap();
+        let ff_2 = FFE::new(10, 1, 5).unwrap();
         let new_ff = ff_1 + ff_2;
-        assert_eq!(new_ff.unwrap(), FF{value: 4, n: 5});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 4,
+                n: 5,
+                g: 1
+            }
+        );
 
-        let ff_3 = FF::new(67, 7).unwrap();
-        let ff_4 = FF::new(60, 7).unwrap();
+        let ff_3 = FFE::new(67, 1, 7).unwrap();
+        let ff_4 = FFE::new(60, 1, 7).unwrap();
         let new_ff = ff_3 + ff_4;
-        assert_eq!(new_ff.unwrap(), FF{value: 1, n: 7});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 1,
+                n: 7,
+                g: 1
+            }
+        );
 
-        let ff_5 = FF::new(67, 7).unwrap();
-        let ff_6 = FF::new(60, 13).unwrap();
+        let ff_5 = FFE::new(67, 1, 7).unwrap();
+        let ff_6 = FFE::new(60, 1, 13).unwrap();
         let new_ff = ff_5 + ff_6;
         assert!(new_ff.is_err());
     }
 
     #[test]
     fn mul() {
-        let ff_1 = FF::new(19, 5).unwrap();
-        let ff_2 = FF::new(11, 5).unwrap();
+        let ff_1 = FFE::new(19, 1, 5).unwrap();
+        let ff_2 = FFE::new(11, 1, 5).unwrap();
         let new_ff = ff_1 * ff_2;
-        assert_eq!(new_ff.unwrap(), FF{value: 4, n: 5});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 4,
+                n: 5,
+                g: 1
+            }
+        );
 
-        let ff_3 = FF::new(67, 7).unwrap();
-        let ff_4 = FF::new(4, 7).unwrap();
+        let ff_3 = FFE::new(67, 1, 7).unwrap();
+        let ff_4 = FFE::new(4, 1, 7).unwrap();
         let new_ff = ff_3 * ff_4;
-        assert_eq!(new_ff.unwrap(), FF{value: 2, n: 7});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 2,
+                n: 7,
+                g: 1
+            }
+        );
 
-        let ff_5 = FF::new(67, 7).unwrap();
-        let ff_6 = FF::new(60, 13).unwrap();
+        let ff_5 = FFE::new(67, 1, 7).unwrap();
+        let ff_6 = FFE::new(60, 1, 13).unwrap();
         let new_ff = ff_5 * ff_6;
         assert!(new_ff.is_err());
     }
 
     #[test]
     fn sub() {
-        let ff_1 = FF::new(19, 5).unwrap();
-        let ff_2 = FF::new(7, 5).unwrap();
+        let ff_1 = FFE::new(19, 1, 5).unwrap();
+        let ff_2 = FFE::new(7, 1, 5).unwrap();
         let new_ff = ff_1 - ff_2;
-        assert_eq!(new_ff.unwrap(), FF{value: 2, n: 5});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 2,
+                n: 5,
+                g: 1
+            }
+        );
 
-        let ff_3 = FF::new(2, 7).unwrap();
-        let ff_4 = FF::new(11, 7).unwrap();
+        let ff_3 = FFE::new(2, 1, 7).unwrap();
+        let ff_4 = FFE::new(11, 1, 7).unwrap();
         let new_ff = ff_3 - ff_4;
-        assert_eq!(new_ff.unwrap(), FF{value: 5, n: 7});
+        assert_eq!(
+            new_ff.unwrap(),
+            FFE {
+                value: 5,
+                n: 7,
+                g: 1
+            }
+        );
 
-        let ff_5 = FF::new(67, 7).unwrap();
-        let ff_6 = FF::new(60, 13).unwrap();
+        let ff_5 = FFE::new(67, 1, 7).unwrap();
+        let ff_6 = FFE::new(60, 1, 13).unwrap();
         let new_ff = ff_5 - ff_6;
         assert!(new_ff.is_err());
     }
